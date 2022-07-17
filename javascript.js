e7f7794685fe26e4ec1018cfa9f0a04212f095d7
T Jessop <tjessop602@gmail.com>
2022-07-18 11:17:20 +1200

    selections are now displayed on screen, not just in terminal.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 function computerChoose(){
-    var array = ["rock", "paper", "scissors"];
-    let computerChoice = Math.floor(Math.random()*4);
+    var array = ["Rock", "Paper", "Scissors"];
+    let computerChoice = Math.floor(Math.random()*3);
     return array[computerChoice];
 }
 
 function playRound(userChoice, computerChoice){
     if(userChoice === computerChoice){
         result = "Draw";
-    }else if((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")){
+    }else if((userChoice === "Rock" && computerChoice === "Scissors") || (userChoice === "Paper" && computerChoice === "Rock") || (userChoice === "Scissors" && computerChoice === "Paper")){
         result = "User Wins";
     }else{
         result = "Computer Wins";
@@ -16,14 +16,26 @@ function playRound(userChoice, computerChoice){
 }
 
 const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    var user = button.id;
-    var computer = computerChoose();
-    var result = playRound(user, computer);
-    console.log("User played " + user + ". Computer played " + computer + ".\n" + result + "\n");
-  });
+
+    button.addEventListener('click', () => {
+        const user = button.id;
+        button.classList.add('clicked', 'disabled'); 
+        document.getElementById('0').textContent = user;
+        var computer = computerChoose();
+        document.getElementById('1').textContent = computer;
+        var result = playRound(user, computer);
+        console.log("User played " + user + ". Computer played " + computer + ".\n" + result + "\n");
+
+    });
+    
+    button.addEventListener('transitionend', () => {
+        button.classList.remove('clicked');
+        setTimeout(function(){
+            button.classList.remove('disabled');
+        }, 2000);
+
+        console.log("remove");
+    });
 });
-
-
-
